d9effbad3aba12b332b07143218d01e0dfb8e165
ToteADS <toteads@gmail.com>
2022-07-11 15:12:49 -0300

    criado a pagina de error

--- lib/app/core/pages/errors_page.dart
+++ lib/app/core/pages/errors_page.dart
@@ -7,8 +7,25 @@ class ErrorsPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            image: const DecorationImage(
+                image: AssetImage(
+                  "assets/images/dead-by-daylight.png",
+                ),
+                fit: BoxFit.cover),
+          ),
+          child: SizedBox(
+            height: 400,
+            width: 400,
+            child: Image.asset("assets/images/connection-error.png"),
+          ),
+        ),
+      ),
+    );
   }
 }
