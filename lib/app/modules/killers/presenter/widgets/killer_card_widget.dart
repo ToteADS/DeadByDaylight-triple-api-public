aa741a6e0ebbdf8af26464144ae5357dd744f80d
ToteADS <toteads@gmail.com>
2022-07-11 14:36:32 -0300

    adicionado background nos cards de killer e surv

--- lib/app/modules/killers/presenter/widgets/killer_card_widget.dart
+++ lib/app/modules/killers/presenter/widgets/killer_card_widget.dart
@@ -10,74 +10,88 @@ class KillerCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.transparent,
-              child: FadeInImage.assetNetwork(
-                image: killer.icon?.portrait ?? CommonVariables.imageDeadByDaylightPattern,
-                placeholder: 'assets/images/grey-background.png',
-                fit: BoxFit.cover,
-                fadeInDuration: const Duration(seconds: 1),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: const DecorationImage(
+              image: AssetImage(
+                "assets/images/killers.png",
               ),
-            ),
-            title: Text(killer.name ?? ""),
-            subtitle: Text(
-              killer.isFree ?? false ? "Free of charge" : "Paid out",
-              style: TextStyle(color: Colors.white.withOpacity(0.6)),
-            ),
-          ),
-          SizedBox(
-            height: 400,
-            child: FadeInImage.assetNetwork(
-              image: killer.icon?.previewPortrait ?? CommonVariables.imageDeadByDaylightPattern,
-              placeholder: 'assets/images/grey-background.png',
-              fit: BoxFit.cover,
-              fadeInDuration: const Duration(seconds: 1),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              killer.overview ?? "",
-              style: TextStyle(color: Colors.white.withOpacity(0.6)),
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.start,
+              fit: BoxFit.cover),
+        ),
+        child: Card(
+          color: Colors.transparent,
+          clipBehavior: Clip.antiAlias,
+          child: Column(
             children: [
-              TextButton(
-                style: TextButton.styleFrom(textStyle: const TextStyle(color: Color(0xFF6200EE))),
-                onPressed: () {
-                  Modular.to.pushNamed("/killers/killer", arguments: killer);
-                },
-                child: const Text(
-                  'Details',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    shadows: <Shadow>[
-                      Shadow(
-                        offset: Offset(1.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.grey,
-                      ),
-                      Shadow(
-                        offset: Offset(2.0, 1.0),
-                        blurRadius: 5.0,
-                        color: Colors.grey,
-                      ),
-                    ],
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  child: FadeInImage.assetNetwork(
+                    image: killer.icon?.portrait ?? CommonVariables.imageDeadByDaylightPattern,
+                    placeholder: 'assets/images/grey-background.png',
+                    fit: BoxFit.cover,
+                    fadeInDuration: const Duration(seconds: 1),
                   ),
                 ),
+                title: Text(killer.name ?? ""),
+                subtitle: Text(
+                  killer.isFree ?? false ? "Free of charge" : "Paid out",
+                  style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                ),
+              ),
+              SizedBox(
+                height: 400,
+                child: FadeInImage.assetNetwork(
+                  image: killer.icon?.previewPortrait ?? CommonVariables.imageDeadByDaylightPattern,
+                  placeholder: 'assets/images/grey-background.png',
+                  fit: BoxFit.cover,
+                  fadeInDuration: const Duration(seconds: 1),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  killer.overview ?? "",
+                  style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(textStyle: const TextStyle(color: Color(0xFF6200EE))),
+                    onPressed: () {
+                      Modular.to.pushNamed("/killers/killer", arguments: killer);
+                    },
+                    child: const Text(
+                      'Details',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(1.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Colors.grey,
+                          ),
+                          Shadow(
+                            offset: Offset(2.0, 1.0),
+                            blurRadius: 5.0,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/app/modules/survivors/presenter/widgets/survivor_card_widget.dart
+++ lib/app/modules/survivors/presenter/widgets/survivor_card_widget.dart
@@ -13,74 +13,88 @@ class SurvivorCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.transparent,
-              child: FadeInImage.assetNetwork(
-                image: survivor.icon?.portrait ?? CommonVariables.imageDeadByDaylightPattern,
-                placeholder: 'assets/images/grey-background.png',
-                fit: BoxFit.cover,
-                fadeInDuration: const Duration(seconds: 1),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: const DecorationImage(
+              image: AssetImage(
+                "assets/images/survivors.png",
               ),
-            ),
-            title: Text(survivor.name ?? ""),
-            subtitle: Text(
-              survivor.isFree ?? false ? "Free of charge" : "Paid out",
-              style: TextStyle(color: Colors.white.withOpacity(0.6)),
-            ),
-          ),
-          SizedBox(
-            height: 400,
-            child: FadeInImage.assetNetwork(
-              image: survivor.icon?.previewPortrait ?? CommonVariables.imageDeadByDaylightPattern,
-              placeholder: 'assets/images/grey-background.png',
-              fit: BoxFit.cover,
-              fadeInDuration: const Duration(seconds: 1),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              survivor.overview ?? "",
-              style: TextStyle(color: Colors.white.withOpacity(0.6)),
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.start,
+              fit: BoxFit.cover),
+        ),
+        child: Card(
+          color: Colors.transparent,
+          clipBehavior: Clip.antiAlias,
+          child: Column(
             children: [
-              TextButton(
-                style: TextButton.styleFrom(textStyle: const TextStyle(color: Color(0xFF6200EE))),
-                onPressed: () {
-                  Modular.to.pushNamed("/survivors/survivor", arguments: survivor);
-                },
-                child: const Text(
-                  'Details',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    shadows: <Shadow>[
-                      Shadow(
-                        offset: Offset(1.0, 2.0),
-                        blurRadius: 3.0,
-                        color: Colors.grey,
-                      ),
-                      Shadow(
-                        offset: Offset(2.0, 1.0),
-                        blurRadius: 5.0,
-                        color: Colors.grey,
-                      ),
-                    ],
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  child: FadeInImage.assetNetwork(
+                    image: survivor.icon?.portrait ?? CommonVariables.imageDeadByDaylightPattern,
+                    placeholder: 'assets/images/grey-background.png',
+                    fit: BoxFit.cover,
+                    fadeInDuration: const Duration(seconds: 1),
                   ),
                 ),
+                title: Text(survivor.name ?? ""),
+                subtitle: Text(
+                  survivor.isFree ?? false ? "Free of charge" : "Paid out",
+                  style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                ),
+              ),
+              SizedBox(
+                height: 400,
+                child: FadeInImage.assetNetwork(
+                  image: survivor.icon?.previewPortrait ?? CommonVariables.imageDeadByDaylightPattern,
+                  placeholder: 'assets/images/grey-background.png',
+                  fit: BoxFit.cover,
+                  fadeInDuration: const Duration(seconds: 1),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  survivor.overview ?? "",
+                  style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(textStyle: const TextStyle(color: Color(0xFF6200EE))),
+                    onPressed: () {
+                      Modular.to.pushNamed("/survivors/survivor", arguments: survivor);
+                    },
+                    child: const Text(
+                      'Details',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(1.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Colors.grey,
+                          ),
+                          Shadow(
+                            offset: Offset(2.0, 1.0),
+                            blurRadius: 5.0,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
